15175fef79cab24e365ce0807574ca7688d9c129
Mirzalfayyadh <mirza.alfayyadh@mail.ugm.ac.id>
2024-11-11 15:06:44 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
+import React from 'react';
+import Sidebar from './components/sidebar/sidebar.jsx';
 
-export default function App() {
-
+function App() {
   return (
-    <>
-    <h1 className="text-3xl font-bold underline">
-      Hello world!  
-    </h1>
-    </>
-  )
+    <div>
+      <Sidebar />
+    </div>
+  );
 }
 
-
+export default App;
